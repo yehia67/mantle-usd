5345a3bb0ba80e8db4c0d8ccc7187e4fedca772c
Yehia Tarek <yehia@nethermind.io>
2025-12-21 09:24:26 +0200

    feat: support add and remove collaterals

--- smart-contracts/mUSD/src/mUSD.sol
+++ smart-contracts/mUSD/src/mUSD.sol
@@ -2,11 +2,28 @@
 pragma solidity ^0.8.20;
 
 import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
 
 /// @title Mantle USD (mUSD)
-/// @notice Minimal ERC20 token with owner-controlled mint and burn capabilities.
+/// @notice ERC20 stablecoin with owner controls plus collateral-backed mint/burn flows.
 contract mUSD is ERC20, Ownable {
+    uint256 private constant BPS_DENOMINATOR = 10_000;
+    uint256 private constant PRICE_SCALE = 1e18;
+
+    IERC20 public collateralAsset;
+    uint256 public mintPercentageBps;
+    uint256 public collateralPriceUsd;
+
+    mapping(address => uint256) public collateralBalances;
+    mapping(address => uint256) public debtBalances;
+
+    event CollateralAssetUpdated(address indexed asset);
+    event MintPercentageUpdated(uint256 bps);
+    event CollateralPriceUpdated(uint256 price);
+    event CollateralLocked(address indexed account, uint256 collateralAmount, uint256 mintedAmount);
+    event CollateralUnlocked(address indexed account, uint256 collateralAmount, uint256 burnedAmount);
+
     constructor() ERC20("Mantle USD", "mUSD") Ownable(msg.sender) {}
 
     function mint(address to, uint256 amount) external onlyOwner {
@@ -15,5 +32,62 @@ contract mUSD is ERC20, Ownable {
 
     function burn(address from, uint256 amount) external onlyOwner {
         _burn(from, amount);
+    }
+
+    function setCollateralAsset(address newCollateral) external onlyOwner {
+        require(newCollateral != address(0), "collateral zero");
+        collateralAsset = IERC20(newCollateral);
+        emit CollateralAssetUpdated(newCollateral);
+    }
+
+    function setMintPercentageBps(uint256 newPercentage) external onlyOwner {
+        require(newPercentage <= BPS_DENOMINATOR, "percentage too high");
+        mintPercentageBps = newPercentage;
+        emit MintPercentageUpdated(newPercentage);
+    }
+
+    /// @notice Sets the collateral price in USD terms (1e18 precision). TODO: replace with oracle feed.
+    function setCollateralPriceUsd(uint256 price) external onlyOwner {
+        require(price > 0, "price zero");
+        collateralPriceUsd = price;
+        emit CollateralPriceUpdated(price);
+    }
+
+    function lockCollateral(uint256 amount) external {
+        require(address(collateralAsset) != address(0), "collateral not set");
+        require(mintPercentageBps > 0, "percentage not set");
+        require(collateralPriceUsd > 0, "price not set");
+        require(amount > 0, "amount zero");
+
+        collateralAsset.transferFrom(msg.sender, address(this), amount);
+
+        uint256 collateralValueUsd = (amount * collateralPriceUsd) / PRICE_SCALE;
+        uint256 mintAmount = (collateralValueUsd * mintPercentageBps) / BPS_DENOMINATOR;
+        require(mintAmount > 0, "mint zero");
+
+        collateralBalances[msg.sender] += amount;
+        debtBalances[msg.sender] += mintAmount;
+
+        _mint(msg.sender, mintAmount);
+
+        emit CollateralLocked(msg.sender, amount, mintAmount);
+    }
+
+    function unlockCollateral(uint256 amount) external {
+        require(amount > 0, "amount zero");
+        require(collateralBalances[msg.sender] >= amount, "insufficient collateral");
+
+        uint256 collateralValueUsd = (amount * collateralPriceUsd) / PRICE_SCALE;
+        uint256 burnAmount = (collateralValueUsd * mintPercentageBps) / BPS_DENOMINATOR;
+        require(burnAmount > 0, "burn zero");
+        require(debtBalances[msg.sender] >= burnAmount, "insufficient debt");
+
+        collateralBalances[msg.sender] -= amount;
+        debtBalances[msg.sender] -= burnAmount;
+
+        _burn(msg.sender, burnAmount);
+        collateralAsset.transfer(msg.sender, amount);
+
+        emit CollateralUnlocked(msg.sender, amount, burnAmount);
     }
 }
--- smart-contracts/mUSD/test/mUSD.t.sol
+++ smart-contracts/mUSD/test/mUSD.t.sol
@@ -4,15 +4,59 @@ pragma solidity ^0.8.20;
 import "forge-std/Test.sol";
 
 import "src/mUSD.sol";
+import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+
+contract MockCollateral is ERC20 {
+    constructor() ERC20("Mantle Ether", "mETH") {}
+
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
+}
 
 contract TestMUSD is Test {
     mUSD token;
+    MockCollateral collateral;
+    address user = address(0xBEEF);
+
+    uint256 constant COLLATERAL_PRICE = 4_000e18; // $4,000 with 1e18 precision
+    uint256 constant MINT_PERCENTAGE = 5_000; // 50% expressed in basis points
 
     function setUp() public {
         token = new mUSD();
+        collateral = new MockCollateral();
+
+        token.setCollateralAsset(address(collateral));
+        token.setMintPercentageBps(MINT_PERCENTAGE);
+        token.setCollateralPriceUsd(COLLATERAL_PRICE);
+
+        collateral.mint(user, 100 ether);
+        vm.prank(user);
+        collateral.approve(address(token), type(uint256).max);
     }
 
     function testNameIsMantleUSD() public {
         assertEq(token.name(), "Mantle USD");
+    }
+
+    function testLockAndUnlockCollateralFlow() public {
+        uint256 collateralToLock = 100 ether;
+
+        vm.prank(user);
+        token.lockCollateral(collateralToLock);
+
+        uint256 expectedMint = (collateralToLock * COLLATERAL_PRICE / 1e18) * MINT_PERCENTAGE / 10_000;
+
+        assertEq(token.balanceOf(user), expectedMint, "incorrect mUSD minted");
+        assertEq(token.collateralBalances(user), collateralToLock, "collateral tracking mismatch");
+        assertEq(token.debtBalances(user), expectedMint, "debt tracking mismatch");
+
+        vm.prank(user);
+        token.unlockCollateral(collateralToLock);
+
+        assertEq(token.balanceOf(user), 0, "mUSD should be burned");
+        assertEq(token.collateralBalances(user), 0, "collateral balance should reset");
+        assertEq(token.debtBalances(user), 0, "debt should reset");
+        assertEq(collateral.balanceOf(user), 100 ether, "collateral should be returned");
     }
 }
