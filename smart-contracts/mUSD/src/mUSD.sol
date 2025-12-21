9b60ef8dd55f26a3ce4393629a02e5b029434215
Yehia Tarek <yehia@nethermind.io>
2025-12-21 14:55:51 +0200

    feat: add liquidation mechanism with health factor monitoring

--- smart-contracts/mUSD/src/mUSD.sol
+++ smart-contracts/mUSD/src/mUSD.sol
@@ -10,10 +10,12 @@ import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
 contract mUSD is ERC20, Ownable {
     uint256 private constant BPS_DENOMINATOR = 10_000;
     uint256 private constant PRICE_SCALE = 1e18;
+    uint256 public constant MIN_HEALTH_FACTOR = 1_500; // 1.5x (150% collateralization minimum)
 
     IERC20 public collateralAsset;
     uint256 public mintPercentageBps;
     uint256 public collateralPriceUsd;
+    uint256 public minHealthFactor = 1_500;
 
     mapping(address => uint256) public collateralBalances;
     mapping(address => uint256) public debtBalances;
@@ -21,8 +23,10 @@ contract mUSD is ERC20, Ownable {
     event CollateralAssetUpdated(address indexed asset);
     event MintPercentageUpdated(uint256 bps);
     event CollateralPriceUpdated(uint256 price);
+    event MinHealthFactorUpdated(uint256 newFactor);
     event CollateralLocked(address indexed account, uint256 collateralAmount, uint256 mintedAmount);
     event CollateralUnlocked(address indexed account, uint256 collateralAmount, uint256 burnedAmount);
+    event PositionLiquidated(address indexed account, uint256 collateralSeized, uint256 debtBurned);
 
     constructor() ERC20("Mantle USD", "mUSD") Ownable(msg.sender) {}
 
@@ -51,6 +55,12 @@ contract mUSD is ERC20, Ownable {
         require(price > 0, "price zero");
         collateralPriceUsd = price;
         emit CollateralPriceUpdated(price);
+    }
+
+    function setMinHealthFactor(uint256 newFactor) external onlyOwner {
+        require(newFactor > 0, "factor zero");
+        minHealthFactor = newFactor;
+        emit MinHealthFactorUpdated(newFactor);
     }
 
     function lockCollateral(uint256 amount) external {
@@ -85,9 +95,58 @@ contract mUSD is ERC20, Ownable {
         collateralBalances[msg.sender] -= amount;
         debtBalances[msg.sender] -= burnAmount;
 
+        uint256 remainingCollateralValue = (collateralBalances[msg.sender] * collateralPriceUsd) / PRICE_SCALE;
+        uint256 remainingDebt = debtBalances[msg.sender];
+        if (remainingDebt > 0) {
+            uint256 healthFactor = (remainingCollateralValue * BPS_DENOMINATOR) / remainingDebt;
+            require(healthFactor >= minHealthFactor, "health factor too low");
+        }
+
         _burn(msg.sender, burnAmount);
         collateralAsset.transfer(msg.sender, amount);
 
         emit CollateralUnlocked(msg.sender, amount, burnAmount);
+    }
+
+    function liquidate(address account) external {
+        require(account != address(0), "account zero");
+        require(collateralBalances[account] > 0, "no collateral");
+        require(debtBalances[account] > 0, "no debt");
+
+        uint256 collateral = collateralBalances[account];
+        uint256 debt = debtBalances[account];
+        uint256 collateralValueUsd = (collateral * collateralPriceUsd) / PRICE_SCALE;
+        uint256 requiredCollateralValue = (debt * BPS_DENOMINATOR) / mintPercentageBps;
+
+        require(collateralValueUsd < requiredCollateralValue, "position healthy");
+
+        collateralBalances[account] = 0;
+        debtBalances[account] = 0;
+
+        _burn(msg.sender, debt);
+
+        emit PositionLiquidated(account, collateral, debt);
+    }
+
+    function withdrawLiquidatedCollateral(uint256 amount) external onlyOwner {
+        collateralAsset.transfer(msg.sender, amount);
+    }
+
+    function getHealthFactor(address account) external view returns (uint256) {
+        uint256 debt = debtBalances[account];
+        if (debt == 0) return type(uint256).max;
+
+        uint256 collateralValueUsd = (collateralBalances[account] * collateralPriceUsd) / PRICE_SCALE;
+        return (collateralValueUsd * BPS_DENOMINATOR) / debt;
+    }
+
+    function isLiquidatable(address account) external view returns (bool) {
+        uint256 collateral = collateralBalances[account];
+        uint256 debt = debtBalances[account];
+        if (debt == 0 || collateral == 0) return false;
+
+        uint256 collateralValueUsd = (collateral * collateralPriceUsd) / PRICE_SCALE;
+        uint256 requiredCollateralValue = (debt * BPS_DENOMINATOR) / mintPercentageBps;
+        return collateralValueUsd < requiredCollateralValue;
     }
 }
